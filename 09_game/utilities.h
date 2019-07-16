6f79cc3b48131625fc6b254cc7ce2a699604eb6d
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-07-15 21:23:41 -0700

    implement a circular buffer container

--- 09_game/utilities.h
+++ 09_game/utilities.h
@@ -15,3 +15,52 @@ enum PaletteColor {
 SDL_Texture	 *  loadImage(const char * filename, SDL_Renderer * renderer, Logger * log = nullptr);
  // load a single image, returning nullptr on fail
  // if logger is passed (optional), then more specific SDL_Image errors will be logged.
+
+
+
+// --- A circular buffer container ---
+
+// Forward declaration
+template <class T> class CircleBufferIter;
+
+template <class T> class CircleBuffer{
+
+  friend class CircleBufferIter<T>;
+
+  typedef CircleBufferIter<T> iterator;
+  typedef ptrdiff_t difference_type;
+  typedef size_t size_type;
+  typedef T value_type;
+  typedef T * pointer;
+  typedef T & reference;
+
+  public:
+    CircleBuffer(int length, T initial_val) : length(length) {arr = new T [length]; for (int i=0; i<length; i++) arr[i]=initial_val;}
+    ~CircleBuffer() {delete[] arr;}
+    void push(T item) {arr[head]=item; head = (head+1)%length;}
+
+    iterator begin() {return iterator(this, 0);}
+    iterator end() {return iterator(this, length);}
+
+  private:
+    int length;
+    int head = 0;
+    T * arr = nullptr;
+};
+
+template <class T> class CircleBufferIter{
+  public:
+    CircleBufferIter(CircleBuffer<T> * circleBuffer, int index) : theCircleBuffer(circleBuffer), index(index) {}
+    CircleBufferIter(const CircleBufferIter<T> & other) {index = other.index; theCircleBuffer=other.theCircleBuffer;}
+    bool operator!=(const CircleBufferIter<T> & other) const {return index != other.index;}
+    T & operator*() {int i = theCircleBuffer->head - index-1; i = i>=0 ? i : i+theCircleBuffer->length ; return theCircleBuffer->arr[i];}
+    CircleBufferIter<T> & operator++() {index++; return *this;}
+    CircleBufferIter<T> & operator++(int dummy) {CircleBufferIter<T> copy(*this); ++(*this); return copy;}
+
+  private:
+    CircleBuffer<T> * theCircleBuffer;
+    int index;
+
+};
+
+// --- end of circular buffer container ---
